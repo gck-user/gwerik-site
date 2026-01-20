71744e6b2563f6f5bdc5665928656bceddda17c2
gwerik admin <get.cloud.knowledge@gmail.com>
2026-01-20 17:57:53 +0530

    new repo name

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Gwerik</h1>
+      <h1>Gwerik Home</h1>
 
       <p>
         Automation • OCI • Architecture • PeopleSoft
